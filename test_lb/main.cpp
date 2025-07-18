6154636dffb8949739bff723b27eef8af72eb1bf
AkromAbdaliev <akromjon996@gmail.com>
2025-07-18 06:10:06 +0500

    restored files

---
+++ test_lb/main.cpp
@@ -0,0 +1,30 @@
+#include "Student.h"
+#include "EngineeringStudent.h"
+#include "GraduatingStudent.h"
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::endl;
+
+int main(){
+    Student student1;
+    student1.setFullName("Jack Black");
+    student1.setStudentNumber(2221133);
+    student1.printStudentInfo();
+
+    EngineeringStudent engineer;
+    engineer.setFullName("Marie Smith");
+    engineer.setDepartment("Electrical");
+    engineer.setStudentNumber(2221111);
+    engineer.printStudentInfo();
+
+    EngineeringStudent engineer1("Jane Smith", 2229999 ,"Computer");
+    engineer1.printStudentInfo();
+
+    GraduatingStudent graduate("Dior Humbre", 2112211 ,"Software", "AA10101011");
+    graduate.printStudentInfo();
+
+
+    return 0;
+}
